1f355a3768abcd4f30aadf6ac9b8197c92cde359
gloryofrobots <gloryofrobots@gmail.com>
2018-02-10 21:25:32 +0200

    delete unrelated

--- web-terminal/scm/stdlib/for.scm
+++
@@ -1,8 +0,0 @@
-(begin 
-    (define (for-each callable lst)
-        (define ret (callable (car lst)))
-        (if (cdr lst) (for-each callable (cdr lst)) ret))
-
-
-    (defmacro (for x in l . calls) (for-each (lambda (,x) ,@calls) ,l))
-)
--- web-terminal/scm/stdlib/let-syntax.scm
+++
@@ -1,6 +0,0 @@
-(define-syntax let-syntax 
-    (syntax-rules () 
-    ((let-syntax () body ...) (begin body ...))
-    ((let-syntax ((name transformer)) body ...) (let () (begin (define-syntax name transformer) body ...)))
-    ((let-syntax ((name transformer) more ...) body ...) (let () (begin (define-syntax name transformer) (let-syntax (more ...) body ...))))))
-
