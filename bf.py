304846e737ef5112281bb07030634e92604be2ee
gloryofrobots <gloryofrobots@gmail.com>
2018-07-30 23:19:58 +0300

    bf

--- bf.py
+++ bf.py
@@ -1,9 +1,38 @@
 import sys
+
+
+" ++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+"++++++++++[>+++++++>++++++++++>+++<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+."
 PROG = """
 " ++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
 """
 
-" ++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+RIGHT = 0
+LEFT = 1
+INC = 2
+DEC = 3
+LOOP_OPEN = 4
+LOOP_CLOSE = 5
+PUTC= 6
+GETC = 7
+
+OP_BITS = 3
+ARG_BITS = 32 - OP_BITS
+OP_MAX = (1 << OP_BITS) - 1
+ARG_MAX = (1 << ARG_BITS) - 1
+OP_MASK = OP_MAX << ARG_BITS
+ARG_MASK = ARG_MAX
+
+def pack(op, arg):
+    return (op << ARG_BITS) + arg
+
+
+def unpack_op(code):
+    return (code & OP_MASK) >> ARG_BITS
+
+def unpack_arg(code):
+    return code & ARG_MASK
+
  # ++[>-]
 SYNTAX =  [">", "<", "+", "-", ".", ",", "[", "]"]
 EOF = -1
@@ -12,73 +41,84 @@ class BF:
     def __init__(self):
         self.data = [0] * 30000
         self.code = []
+        self.size = 0
         self.dp = 0
         self.cp = 0
 
     def run(self, txt):
-        self.code = list(filter(lambda c: c in SYNTAX, [c for c in txt]))
-        self.code.append(EOF)
+        self.compile(txt)
         self.eval()
 
-    def eval(self):
-        self._eval(EOF)
+    def compile(self, txt):
+        txt = list(filter(lambda c: c in SYNTAX, [c for c in txt]))
 
-    def _eval(self, goto):
-        while True:
-            op = self.code[self.cp]
-            # print("op", self.cp, op, goto)
-            if op == ">":
+        self.size = len(txt)
+        self.code = [0] * self.size
+
+        jumps = []
+        for i in range(self.size):
+            ch = txt[i]
+            arg = 0
+            if ch == ">":
+                op = RIGHT
+            elif ch == "<":
+                op = LEFT
+            elif ch == "+":
+                op = INC
+            elif ch =="-":
+                op = DEC
+            elif ch == ".":
+                op = PUTC
+            elif ch == ",":
+                op = GETC
+            elif ch == "[":
+                op = LOOP_OPEN
+                jump = i
+                # print("LO", jump)
+                jumps.append(jump)
+
+            elif ch == "]":
+                op = LOOP_CLOSE
+                arg = jumps.pop()
+                # print("LC",  arg, i)
+                self.code[arg] = pack(LOOP_OPEN, i)
+                
+            self.code[i] = pack(op, arg)
+        # sys.exit(-1)
+
+    def eval(self):
+        while self.cp < self.size:
+            code = self.code[self.cp]
+            op = unpack_op(code)
+            arg = unpack_arg(code)
+            
+            # print("op", self.cp, op, arg)
+
+            if op == RIGHT:
                 self.dp += 1
-            elif op == "<":
+            elif op == LEFT:
                 self.dp -= 1
-            elif op == "+":
+            elif op == INC:
                 self.add(1)
-            elif op == "-":
+            elif op == DEC:
                 self.add(-1)
 
-            elif op == ".":
+            elif op == PUTC:
                 sys.stdout.write(chr(self.get()))
                 sys.stdout.flush()
-            elif op == ",":
+            elif op == GETC:
                 ch = sys.stdin.read(1)
                 self.set(ord(ch))
 
-            elif op == "[":
+            elif op == LOOP_OPEN:
                 # print ("LOOP")
                 if self.get() == 0:
-                    # print("SKIPPING")
-                    loop_count = 1
-                    self.cp += 1
-                    while True:
-                        op = self.code[self.cp]
-                        if op == "]":
-                            loop_count -= 1
-                            # print ("LOOP CLOSED")
-                            if loop_count == 0:
-                                break
-                        elif op == EOF:
-                            raise SyntaxError("UNCLOSED LOOP")
-                        elif op == "[":
-                            loop_count += 1
-
-                        self.cp += 1
-                    
-                else:
-                    print("ENTER LOOP")
-                    self.cp += 1
-                    goto = self._eval(self.cp)
-
-            elif op == "]":
+                    self.cp = arg
+            elif op == LOOP_CLOSE:
                 if self.get() != 0:
-                    if goto == EOF:
-                        raise SyntaxError("INVALID LOOP CONDITION")
-                    # print("JUMP TO", goto)
-                    self.cp = goto
-                    return goto
-                
-            elif op == EOF:
-                sys.stdout.flush()
-                return
+                    # print("JUMP TO", self.get(), arg)
+                    self.cp = arg
+                    
             self.cp += 1
 
     def get(self):
@@ -93,3 +133,6 @@ class BF:
 
 bf = BF()
 bf.run(PROG)
+# c  = pack(7, 128)
+# print(unpack_op(c))
+# print(unpack_arg(c))
